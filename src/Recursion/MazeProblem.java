81fcc4885775e488cb809f1b060d52ce1b0ee634
Yogesh Pandey <bc21143@smsvaranasi.in>
2023-12-15 16:07:15 +0530

    maze problem

---
+++ src/Recursion/MazeProblem.java
@@ -0,0 +1,20 @@
+package Recursion;
+
+public class MazeProblem {
+
+    public static void main(String[] args) {
+        System.out.println( count(2 ,2));
+    }
+
+    public static int  count(int r ,int c)
+    {
+        if(r==1||c==1)
+        {
+            return 1;
+
+        }
+        int left =count(r-1,c);
+        int right = count(r,c-1);
+        return left+right;
+    }
+}
--- src/Sort/SelectionSort.java
+++ src/Sort/SelectionSort.java
@@ -15,16 +15,15 @@ public class SelectionSort {
 
         for(int i=0;i<arr.length;i++) {
 
-            int minIdx = i;
+            int minIdx = i; // taking index of 1st
             for (int j = i+1; j < arr.length;j++){
 
-                if(arr[j]<arr[minIdx])
+                if(arr[j]<arr[minIdx])  // checking whether arr[j]<arr[minIdx] while it not get it actual location
                 {
                     minIdx=j;
                 }
-
             }
-            if(minIdx!=i)
+            if(minIdx!=i)      // if minimus index is not equalls to i then swap
             swap(arr,i,minIdx);
         }
     }
@@ -36,3 +35,7 @@ public class SelectionSort {
         arr[i2]=temp;
     }
 }
+
+
+
+
